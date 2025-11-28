089a3e20032599d8349db7fa95c4d1eaa02c4c85
jhaeyzh-a11y <jhaeyzh@gmail.com>
2025-11-28 09:21:45 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,20 +1,55 @@
-const CACHE_NAME = 'wellness-v1';
+const CACHE_NAME = 'wellness-app-v3';
 const urlsToCache = [
-  'https://jhaeyzh-a11y.github.io/quezon-nhs-wellness/',
-  'https://jhaeyzh-a11y.github.io/quezon-nhs-wellness/index.html',
-  'https://jhaeyzh-a11y.github.io/quezon-nhs-wellness/manifest.json'
+  '/quezon-nhs-wellness/',
+  '/quezon-nhs-wellness/index.html',
+  '/quezon-nhs-wellness/manifest.json',
+  '/quezon-nhs-wellness/sw.js'
 ];
 
+// Install event - cache all essential files
 self.addEventListener('install', event => {
+  console.log('Service Worker installing...');
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
+      .then(cache => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+      .then(() => self.skipWaiting())
   );
 });
 
+// Activate event - clean up old caches
+self.addEventListener('activate', event => {
+  console.log('Service Worker activating...');
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            console.log('Deleting old cache:', cacheName);
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    }).then(() => self.clients.claim())
+  );
+});
+
+// Fetch event - serve from cache or network
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
-      .then(response => response || fetch(event.request))
+      .then(response => {
+        // Return cached version or fetch from network
+        return response || fetch(event.request);
+      })
   );
+});
+
+// Message event - for communication with the app
+self.addEventListener('message', event => {
+  if (event.data === 'skipWaiting') {
+    self.skipWaiting();
+  }
 });
