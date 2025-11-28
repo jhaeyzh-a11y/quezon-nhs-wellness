b0735558c4e04b0717e57cf890a743abc21fb26f
jhaeyzh-a11y <jhaeyzh@gmail.com>
2025-11-28 12:42:39 +0800

    Implement service worker with proper caching

--- sw.js
+++ sw.js
@@ -1,46 +1,22 @@
-const CACHE_NAME = 'wellness-app-v3.0';
+const CACHE_NAME = 'wellness-v1';
 const urlsToCache = [
   '/quezon-nhs-wellness/',
   '/quezon-nhs-wellness/index.html',
   '/quezon-nhs-wellness/manifest.json',
-  '/quezon-nhs-wellness/sw.js'
+  '/quezon-nhs-wellness/icon-192.png',
+  '/quezon-nhs-wellness/icon-512.png'
 ];
 
 self.addEventListener('install', (event) => {
-  console.log('Service Worker installing.');
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then((cache) => {
-        console.log('Opened cache');
-        return cache.addAll(urlsToCache);
-      })
-  );
-});
-
-self.addEventListener('activate', (event) => {
-  console.log('Service Worker activating.');
-  event.waitUntil(
-    caches.keys().then((cacheNames) => {
-      return Promise.all(
-        cacheNames.map((cacheName) => {
-          if (cacheName !== CACHE_NAME) {
-            console.log('Deleting old cache:', cacheName);
-            return caches.delete(cacheName);
-          }
-        })
-      );
-    })
+      .then((cache) => cache.addAll(urlsToCache))
   );
 });
 
 self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request)
-      .then((response) => {
-        if (response) {
-          return response;
-        }
-        return fetch(event.request);
-      })
+      .then((response) => response || fetch(event.request))
   );
 });
