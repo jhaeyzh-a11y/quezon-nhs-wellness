0d0e117504b97444b2994d5456a7fb14312c45bc
jhaeyzh-a11y <jhaeyzh@gmail.com>
2025-11-28 12:21:08 +0800

    SW check url

--- sw.js
+++ sw.js
@@ -1,20 +1,13 @@
-const CACHE_NAME = 'wellness-app-v1';
-const urlsToCache = [
-  '/quezon-nhs-wellness/',
-  '/quezon-nhs-wellness/index.html',
-  '/quezon-nhs-wellness/manifest.json'
-];
+const CACHE_NAME = 'wellness-app-v1.2';
+const START_URL = 'https://jhaeyzh-a11y.github.io/quezon-nhs-wellness/';
 
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
-  );
-});
-
-self.addEventListener('fetch', event => {
+// Update all fetch events to handle the new domain
+self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request)
-      .then(response => response || fetch(event.request))
+      .then((response) => {
+        // Return cached version or fetch from network
+        return response || fetch(event.request);
+      })
   );
 });
