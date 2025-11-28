2541bd884fd4bb7c96f874e6021cb193536fa468
jhaeyzh-a11y <jhaeyzh@gmail.com>
2025-11-28 12:36:29 +0800

    Corrected SW

--- sw.js
+++ sw.js
@@ -1,13 +1,46 @@
-const CACHE_NAME = 'wellness-app-v1.2';
-const START_URL = 'https://jhaeyzh-a11y.github.io/quezon-nhs-wellness/';
+const CACHE_NAME = 'wellness-app-v3.0';
+const urlsToCache = [
+  '/quezon-nhs-wellness/',
+  '/quezon-nhs-wellness/index.html',
+  '/quezon-nhs-wellness/manifest.json',
+  '/quezon-nhs-wellness/sw.js'
+];
 
-// Update all fetch events to handle the new domain
+self.addEventListener('install', (event) => {
+  console.log('Service Worker installing.');
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  console.log('Service Worker activating.');
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            console.log('Deleting old cache:', cacheName);
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
 self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request)
       .then((response) => {
-        // Return cached version or fetch from network
-        return response || fetch(event.request);
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
       })
   );
 });
